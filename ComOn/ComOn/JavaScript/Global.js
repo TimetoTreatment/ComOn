9604597b9dd9babc3e987dd506d22697d1b85e40
TimetoTreatment <ygeonho1@naver.com>
2020-06-11 03:58:05 +0900

    # 2.06  - 자바스크립트 추가  - 발표 준비

--- ComOn/ComOn/JavaScript/Global.js
+++ ComOn/ComOn/JavaScript/Global.js
@@ -1,4 +1,11 @@
-var teamMember = '                                    :: Team 5 ::\n\n';
+var teamMember = '\n                                    :: Team 5 ::\n\n';
 
 teamMember += '                      Geon-Ho Yun  &  Hyo-Bi Lim\n';
 teamMember += '                    Young-Eun Kim  &  Eun-Hyuk Woo\n';
+
+function goHome() {
+    var currentDate = new Date();
+    alert(currentDate);
+    var welcomAlert = '                            :: Welcome to ComOn ::'
+    alert(welcomAlert);
+}
