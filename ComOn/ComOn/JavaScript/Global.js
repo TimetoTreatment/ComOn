b1016d3c5118747196749f2904647e460dfc5c70
TimetoTreatment <ygeonho1@naver.com>
2020-06-10 21:56:10 +0900

    # 2.05  - 부제 수정

---
+++ ComOn/ComOn/JavaScript/Global.js
@@ -0,0 +1,4 @@
+var teamMember = '                                    :: Team 5 ::\n\n';
+
+teamMember += '                      Geon-Ho Yun  &  Hyo-Bi Lim\n';
+teamMember += '                    Young-Eun Kim  &  Eun-Hyuk Woo\n';
