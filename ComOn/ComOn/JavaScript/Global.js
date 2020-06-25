f0b5e61e9bc2265c44b2b26546171455b522a347
TimetoTreatment <ygeonho1@naver.com>
2020-06-25 20:26:51 +0900

    # 2.17 ( 주석 추가 )  1. 기본 페이지 주석 작성

--- ComOn/ComOn/JavaScript/Global.js
+++ ComOn/ComOn/JavaScript/Global.js
@@ -1,3 +1,18 @@
+/*-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+// Global.js
+//
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+    개요 : 전체 JavaScript
+    설명 :
+        - 알림 창 출력
+        - HTML과 함께 사용
+*/
+
+/***************/
+/* 환영 메시지 */
+/***************/
 function PrintWelcome() {
 
     var currentDate = new Date();
@@ -7,6 +22,10 @@ function PrintWelcome() {
     alert(welcomeStr);
 }
 
+
+/***********/
+/* 팀 소개 */
+/***********/
 function PrintTeamMember() {
 
     var teamMember = '\n                                    :: Team 5 ::\n\n';
